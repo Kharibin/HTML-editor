8ba7cc09e3f0a4f2e55190903758f1aa98cdc576
Kharybin <ai.kharybin@gmail.com>
2017-09-09 21:14:15 +0300

    0.21
    Fullfiled editorPane initialization method

--- View.java
+++ View.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -41,12 +42,24 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
-
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPaneHTML = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollPaneHTML);
+        JScrollPane jScrollPaneTEXT = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPaneTEXT);
+        TabbedPaneChangeListener editorListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(editorListener);
+        tabbedPane.setPreferredSize(new Dimension(1500,800));
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui() {
         initMenuBar();
         initEditor();
         pack();
+    }
+
+    public void selectedTabChanged(){
+
     }
 }
